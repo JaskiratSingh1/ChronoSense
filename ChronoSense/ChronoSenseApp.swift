98b3deadbcea8be562ba57da5ecbbb0fae16c98a
Jaskirat Singh <jaskirat1999@gmail.com>
2025-01-10 13:40:10 -0700

    Initial Commit

---
+++ ChronoSense/ChronoSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoSenseApp.swift
+//  ChronoSense
+//
+//  Created by Jaskirat Singh on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ChronoSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
